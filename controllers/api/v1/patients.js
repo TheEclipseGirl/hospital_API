bf256c84b6c59e81645e727a620d149340dd9679
Shalini <shalinibvp.02@gmail.com>
2020-09-07 23:38:39 +0530

    Minor Bugs fixed

--- controllers/api/v1/patients.js
+++ controllers/api/v1/patients.js
@@ -7,26 +7,26 @@ const { report } = require('../../../routes/api/v1/patients');
 
 module.exports.register= async function(req,res){
     // Patient Find Using Contact
-    let patient= await (await Patient.findOne({contact:req.body.contact})).populate(reports);
     try{
-    if(patient){
-        return res.json(409,{
-            message:"Patient Already Registered",
-            data:patient
-        });
-    }else{
-       await Patient.create(req.body);
-        return res.json(200,{
-            message:'Patient has been successfully Regestered'
+        let patient= await Patient.findOne({contact:req.body.contact}).populate('reports');
+        if(patient){
+            return res.json(409,{
+                message:"Patient Already Registered",
+                data:patient
+            });
+        }else{
+        await Patient.create(req.body);
+            return res.json(200,{
+                message:'Patient has been successfully Registered'
+            });
+        }
+    }
+    catch(error){
+        console.log(console.error);
+        return res.json(500,{
+            message:'Internal Server Error'
         });
     }
-}
-catch(error){
-    console.log(console.error);
-    return res.json(500,{
-        message:'Internal Server Error'
-    });
-}
 
 
 }
